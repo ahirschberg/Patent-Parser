525d601c716e2c4d70b0b8ad51e2da246e6b766a
ahirschberg <googlestinks@ymail.com>
2014-02-16 18:26:05 -0500

    Xml can now be parsed no matter where it is in the zip, as long as its name = the zip's name. Cleaned up output by using sys.stdout.  Improved getgovtinterest() in cases where there are no line breaks

--- run.py
+++ run.py
@@ -7,54 +7,56 @@ import zipfile
 from bs4 import BeautifulSoup
 
 
-#
-# Change tagList[10] when you add new tags, you big dummy!
-#
-tagList = [ 'document-id>document-date', #Date (published?)
-            'title-of-invention', #Title of invention
-            'subdoc-abstract>paragraph', #Abstract
-            'inventors', #List of inventors
-            'subdoc-description>cross-reference-to-related-applications', #Cross reference to related applications
-            'domestic-filing-data>application-number>doc-number', #Id number
-            'domestic-filing-data>filing-date', #US Date filed
-            #PCT Tags
-            'foreign-priority-data>filing-date', #PCT Filing Date
-            'foreign-priority-data>priority-application-number>doc-number', #PCT Application number
+tagList = [ 'patent-application-publication', # Enclosing tags
+            'document-id>document-date', # Date (published?)
+            'title-of-invention', # Title of invention
+            'subdoc-abstract>paragraph', # Abstract
+            'inventors', # List of inventors
+            'subdoc-description>cross-reference-to-related-applications', # Cross reference to related applications
+            'domestic-filing-data>application-number>doc-number', # Id number
+            'domestic-filing-data>filing-date', # US Date filed
+            # PCT Tags
+            'foreign-priority-data>filing-date', # PCT Filing Date
+            'foreign-priority-data>priority-application-number>doc-number', # PCT Application number
 
-            'cross-reference-to-related-applications>paragraph', #Related Patent Documents
-            'subdoc-description>federal-research-statement>paragraph-federal-research-statement', #Government Interest? - Paragraph acknowledging NSF
-            'continuity-data>division-of' #Parent Case - cases in <parent-child>? 
+            'cross-reference-to-related-applications>paragraph', # Related Patent Documents
+            'subdoc-description>federal-research-statement>paragraph-federal-research-statement', # Government Interest? - Paragraph acknowledging NSF
+            'continuity-data>division-of' # Parent Case - cases in <parent-child>? 
             ]
+cleanupMode = 0 # Cleanup mode for files: 0 is none, 1 will remove all extracted directory trees, and 2 will remove the directory trees and their zip files
 
 logParsed = False # Useful for debugging, will write all parsed text to file, which is good if the full file is too big to be opened
-xmldocs = []
-xmliteration = 0
+
+xmldocs = [] # split_xml saves the split xml lists here 
+xmliteration = 0 # Progression through xmldocs
 
 def main():
     pageurl = 'http://patents.reedtech.com/'
-    print 'Getting webpage'
+    print 'Getting webpage', pageurl
     # Get patent urls from webpage
-    #urls = getUrlList(pageurl + 'parbft.php')
-    urls = [['/pa011227.zip', 'just fakin', 'lol']]
+    urls = getUrlList(pageurl + 'parbft.php')
+    #urls = [['/pa010315.zip', 'emulate', '.']]
     i = len(urls) - 1
     print i
+    # Iterate through urls from oldest to newest, downloading and parsing each one
     while i >= 0:
         print 'i is', i 
         url = urls[i]
-        print 'Getting zip file %s with size %s' % (getUrlFilename(url[0]), url[1])
         if not os.path.exists(getwd() + '/temp/'):
             os.makedirs(getwd() + '/temp/')
-        #response = urllib.urlretrieve(pageurl + url[0], getwd() + '/temp/' + getUrlFilename(url[0]), reporthook)
-        response = (open(getwd() + '/pa011227.zip'), '')
+        response = urllib.urlretrieve(pageurl + url[0], getwd() + '/temp/' + getUrlFilename(url[0]), reporthook)
+        #response = (open(getwd() + url[0]), '')
         print 'Got response:', response
-
+        
+        fulldoc = None
         with zipfile.ZipFile(response[0], 'r') as myzip:
-            fname = getUrlFilename(url[0], True) + '.xml'
-            # Patent application xml is not always in root (no idea why)
-            for root, subFolders, files in os.walk(myzip):
-                for folder in subFolders:
-                    if folder.contains(fname):
-                        fulldoc = myzip.open(fname)
+            xmlname = getUrlFilename(url[0], True) + '.xml'
+            # Patent application xml is not always in root, namelist() gets all files within zip
+            for filename in myzip.namelist():
+                print filename
+                if getUrlFilename(filename) == xmlname:
+                    print 'Found %s in %s' % (xmlname, response[0])
+                    fulldoc = myzip.open(filename)
         
         split_xml(fulldoc)
         if not logParsed:
@@ -109,20 +111,19 @@ def getUrlList(url):
                     # Add the trVal to the result. Although trVal is empty, due to how objects work it will still be affected by the tag/text append statements below
                     link = td.find('a')
                     if str(type(link)) == "<class 'bs4.element.Tag'>": 
-                        print 'Appending', link.get('href')
                         trVal.append(link.get('href'))
                     # If the first <td> is not a url, it must be a heading and should be skipped
                     elif len(trVal) == 0:
                         break
                     # If the data is not a link, then it is either file information or date (in that order), which could be useful
                     else:
-                        print 'Appending', td.text
                         trVal.append(td.text)
                 
                 # If nothing of value was found, do not add the values to the result
                 if len(trVal) > 0:
                     result.append(trVal)
     return result 
+
 
 # The parser will not parse past the second dtd statement, so this will split each xml segment into its own file in memory
 def split_xml(fulldoc):
@@ -133,8 +134,7 @@ def split_xml(fulldoc):
     for line in fulldoc:
         lnum += 1
         xml.append(line)
-        if (line.strip().find('</patent-application-publication>') >= 0):
-            print line
+        if (line.strip().find(formatTag(tagList[0], True)) >= 0):
             # Clone the list and append it to xmldocs
             xmldocs.append(list(xml))
             # Write to file (should be commmented out, for debugging purposes
@@ -143,14 +143,21 @@ def split_xml(fulldoc):
                 f.write(''.join(xml))
             n_iter += 1
             xml = []
-            #If this is uncommented, it will split only a set maximum of times
-            #if (n_iter > 100):
+            sys.stdout.write("\rSplit %d on line %d ..." % (n_iter, lnum))
+            sys.stdout.flush()
+
+            # Debug Break-If this is uncommented, it will split only a set maximum of times
+            #if (n_iter > 10):
             #    return 
-    print 'Done.  Split xml file into %s individual xml docs' % len(xmldocs)
+    print 'Done.'
+
+
+def formatTag(tag, close=False):
+    # Remove the tag tree data from the string and enclose it in <>, with an optional /
+    return  ('</' if close else '<') + tag[ tag.rfind('>') + 1: ] + '>'
 
 
 def scrape_multi(xmldocs):
-    #scrape(xmllist=xmldocs[0])
     for xml in xmldocs:
         scrape(xml)
         global xmliteration
@@ -158,13 +165,13 @@ def scrape_multi(xmldocs):
 
 
 def scrape(xmllist):
-    print 'Scraping %s of %s.' % (xmliteration + 1, len(xmldocs)),
-
+    sys.stdout.write("\rScraping %s of %s." % (xmliteration + 1, len(xmldocs))),
+    sys.stdout.flush()
+    
     # Gets the government interest field and looks for NSF or national science foundation
     if (get_govt_interest(xmllist)):
         print 'Found NSF reference, adding to CSV. <!!!!!!!!!!!'
     else:
-        print 'No NSF reference, skipping'
         return 
     # Create a string from the singular xml list created in split_xml()
     xml = '\n'.join(xmllist)
@@ -179,22 +186,22 @@ def scrape(xmllist):
 
 
 # get Govt Interest without using lxml (prevents a whole xml tree structure from needing to be parsed and created)
-# This makes parsing without finding any NSF clauses (the vast majority) 80.060606061 times faster
 def get_govt_interest(xmllist):
-    startpos = -1
-    endpos = -1
+    opentag = False
     for line in xmllist:
-        startpos2 = line.find('<paragraph-federal-research-statement>')
-        endpos2 = line.find('</paragraph-federal-research-statement>')
-        if startpos2 >= 0:
-            startpos = startpos2
-        if endpos2 >= 0:
-            endpos = endpos2
-        
+        startpos = line.find('<paragraph-federal-research-statement>')
+        endpos = line.find('</paragraph-federal-research-statement>')
         if startpos >= 0:
+            opentag = True
+
+        if opentag == True:
             standardline = re.sub("[^a-zA-Z0-9]", "", line).lower() 
+            
+            # If the start or end pos is on the current line, only look at the portion of the line within the tags
+            if startpos >= 0: standardline = standardline[startpos :]
+            if endpos >= 0: standardline = standardline[: endpos]
+
             if (standardline.find('nsf') >= 0 or standardline.find('nationalsciencefoundation') >= 0):
-                print line
                 return True
         if endpos >= 0:
             return False
@@ -326,7 +333,8 @@ def write_data(datalist):
 
     f.close()
 
-start = time.clock()
-main()
-elapsed = (time.clock() - start)
-print 'Time elapsed:', elapsed
+if __name__ == '__main__':
+    start = time.clock()
+    main()
+    elapsed = (time.clock() - start)
+    print '\nTime elapsed:', elapsed
